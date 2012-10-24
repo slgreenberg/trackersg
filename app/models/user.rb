0b08ac269d94d49664960bfa56bbe69e4806a797
Susan Greenberg <susangr@seas.upenn.edu>
2012-10-24 11:05:50 -0400

    add validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
   has_many :workouts
   attr_accessible :name
+  validates :name, :uniqueness => true
 end
--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,6 +1,8 @@
 class Workout < ActiveRecord::Base
   belongs_to :user
   attr_accessible :activity, :minutes, :name, :user_id, :user
+  validates :activity, :minutes, :name, :presence => true
+  validates_associated :user
   
   def user_name
     user.name if user
